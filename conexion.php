16535764ed54bb460d6f3d7d083e78132815024e
Maria-asir <marrodsan20@alu.edu.gva.es>
2026-05-28 17:12:45 +0200

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host = "192.168.10.12"; // IP del Servidor de Almacenamiento (Red Interna)
-$port = "5432";
-$dbname = "prueba";
-$user = "usuario_web";
-$password = "f56i7o8p";
+$host = ""; // IP del Servidor de Almacenamiento (Red Interna)
+$port = "";
+$dbname = "";
+$user = "";
+$password = "";
 
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
 $dbconn = pg_connect($connection_string);
